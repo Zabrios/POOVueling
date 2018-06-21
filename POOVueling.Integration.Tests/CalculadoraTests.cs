a2968c32d50375e97c5921949b9fdc9e5f393edb
Daniel Ruz <daniel.ruzvaz@gmail.com>
2018-06-21 18:30:34 +0200

    Uploading VS Integration Tests and XUnit Integration and Unit Tests

--- POOVueling.Integration.Tests/CalculadoraTests.cs
+++ POOVueling.Integration.Tests/CalculadoraTests.cs
@@ -11,22 +11,28 @@ namespace POOVueling.Tests
     [TestClass()]
     public class CalculadoraTests
     {
-        [TestMethod()]
-        public void CloneTest()
+        ICalculadora iCalculadora = new Calculadora();
+
+        //[TestMethod()]
+        //public void CloneTest()
+        //{
+        //    Assert.Fail();
+        //}
+
+        [DataRow(4, 2, 2)]
+        [DataRow(9, 3, 3)]
+        [DataTestMethod()]
+        public void DivisionTest(int num1, int num2, int resultado)
         {
-            Assert.Fail();
+            Assert.IsTrue(iCalculadora.Division(num1, num2) == resultado);
         }
 
-        [TestMethod()]
-        public void DivisionTest()
+        [DataRow(4, 2, 8)]
+        [DataRow(6, 3, 18)]
+        [DataTestMethod()]
+        public void MultiplicacionTest(int num1, int num2, int resultado)
         {
-            Assert.Fail();
-        }
-
-        [TestMethod()]
-        public void MultiplicacionTest()
-        {
-            Assert.Fail();
+            Assert.IsTrue(iCalculadora.Multiplicacion(num1, num2) == resultado);
         }
 
         [TestMethod()]
